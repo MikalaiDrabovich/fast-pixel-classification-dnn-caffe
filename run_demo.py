67f476af9c5fa1a53cebf5290ba017ca45467f21
Mikalai Drabovich <nick.drabovich@amd.com>
2017-08-17 18:24:40 -0700

    Update run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -45,25 +45,25 @@ def id2bgr(im):
     color_image = np.empty((w, h, 3), dtype=np.uint8)
     code = """
     unsigned char cityscape_object_colors[19][3] = {
-    {128, 64,128}, // road
+    {128, 64,128},  // road
     {232, 244, 35}, // sidewalk
-    { 70, 70, 70}, // building
+    { 70, 70, 70},  // building
     {156, 102,102}, // wall
-    {190,153,153}, // fence
-    {153,153,153}, // pole
-    {30,170, 250}, // traffic light
+    {190,153,153},  // fence
+    {153,153,153},  // pole
+    {30,170, 250},  // traffic light
     {0, 220,  220}, // traffic sign
-    {35,142, 107}, // vegetation
-    {152,251,152}, // terrain
+    {35,142, 107},  // vegetation
+    {152,251,152},  // terrain
     { 180, 130,70}, // sky
-    {60, 20, 220}, // person
-    {0,  0,  255}, // rider
-    { 142,  0,0}, // car
-    { 70,  0, 0}, // truck
-    {  100, 60,0}, // bus
-    {  100, 80,0}, // train
-    {  230,  0,0}, // motorcycle
-    {32, 11, 119}  // bicycle
+    {60, 20, 220},  // person
+    {0,  0,  255},  // rider
+    { 142,  0,0},   // car
+    { 70,  0, 0},   // truck
+    {  100, 60,0},  // bus
+    {  100, 80,0},  // train
+    {  230,  0,0},  // motorcycle
+    {32, 11, 119}   // bicycle
     };
 
     int impos=0;
@@ -154,7 +154,8 @@ if __name__ == "__main__":
     input_w = 2048
     input_h = 1024
 
-    # # Models with argmax and without upsampling are faster.
+    # # Models with argmax and without upsampling are significantly faster
+    # # but not necessary worse for driving decisions, in part because of reaction time.
     
     # # original model
     # # model_description = './models/deploy_model_with_upsampling_without_argmax.prototxt'
@@ -169,15 +170,17 @@ if __name__ == "__main__":
     # # model_has_argmax = False
     # # 
     # # improved 3, should be the fastest
-    model_description = './models/deploy_model_without_upsampling_with_argmax.prototxt'
-    model_has_argmax = True
-    model_weights = './models/weights.caffemodel'
+    # # model_description = './models/deploy_model_without_upsampling_with_argmax.prototxt'
+    # # model_has_argmax = True
+    # # model_weights = './models/weights.caffemodel'
  
     # comment out these two lines once you copied trained models and descriptions to ./models
     model_weights = './models/this_needs_to_be_provided_by_you.caffemodel'
     model_description = './models/this_needs_to_be_provided_by_you.prototxt'
  
     #--------------------------------------------------------------------
+
+
 
     os.system("./generate_image_list_for_demo.sh")
     image_list_file = open('./image_list_video.txt')
