598101554eaf87f74d01aaf7b6205f5cd269b62a
Mikalai Drabovich <nick.drabovich@amd.com>
2017-08-23 18:58:09 -0700

    Update generate_image_list_for_demo.sh

--- generate_image_list_for_demo.sh
+++ generate_image_list_for_demo.sh
@@ -14,19 +14,15 @@ else
   echo 'Directory "images" exists, skipping download of a quick test dataset"'
 fi
  
-
 rm -f image_list_video.txt
+
 # uncomment if you want to use images from cityscapes demo dataset
 #find images/leftImg8bit/demoVideo/ -name "*.png" -print > image_list_video_temp.txt
 find images/ -name "*.jpg" -print > image_list_video_temp.txt
-
 sort image_list_video_temp.txt > image_list_video.txt
 
 # use the same images to create a longer list of video frames 
 #cat image_list_video.txt > image_list_video_temp.txt
-#cat image_list_video_temp.txt >> image_list_video.txt
-#cat image_list_video_temp.txt >> image_list_video.txt
-#cat image_list_video_temp.txt >> image_list_video.txt
 #cat image_list_video_temp.txt >> image_list_video.txt
 
 rm -f image_list_video_temp.txt
